12d0d6afd1e7982711e0efae7223fb225678b197
wujun <jun.wu@lrlz.com>
2017-12-20 17:08:58 +0800

    1.小改动

--- app/src/main/java/com/zcy/ghost/vivideo/widget/UnScrollViewPager.java
+++ app/src/main/java/com/zcy/ghost/vivideo/widget/UnScrollViewPager.java
@@ -4,17 +4,12 @@ import android.content.Context;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
-import android.widget.Scroller;
-
-import com.zcy.ghost.vivideo.utils.KL;
 
 /**
  * Created by Xuanhao on 2016/3/8.
  */
 public class UnScrollViewPager extends ViewPager {
-
-    private boolean isScrollable = false;
-    private Context mContext;
+    private boolean isScrollable;
 
     public UnScrollViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -22,9 +17,6 @@ public class UnScrollViewPager extends ViewPager {
 
     public UnScrollViewPager(Context context) {
         super(context);
-        this.mContext = context;
-        Scroller scroller = new Scroller(mContext);
-
     }
 
     public void setScrollable(boolean scrollable) {
@@ -33,17 +25,11 @@ public class UnScrollViewPager extends ViewPager {
 
     @Override
     public boolean onTouchEvent(MotionEvent arg0) {
-        if (isScrollable)
-            return super.onTouchEvent(arg0);
-        boolean b = super.onTouchEvent(arg0);
-        KL.e(UnScrollViewPager.class, "onTouchEvent" + b);
-        return false;
+        return isScrollable && super.onTouchEvent(arg0);
     }
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent arg0) {
-        if (isScrollable)
-            return super.onInterceptTouchEvent(arg0);
-        return false;
+        return isScrollable && super.onInterceptTouchEvent(arg0);
     }
 }
